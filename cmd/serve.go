d6d06a4c5fef2f3ffbad9f228db9d814d76c0121
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-07-05 23:28:20 +0100

    docs: update serve command help messages

--- cmd/serve.go
+++ cmd/serve.go
@@ -12,13 +12,7 @@ import (
 
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Start S3-compatible Lab Store server",
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info("Welcome to Lab Store, by https://youtube.com/@DataLabTechTV")
 		log.Debug("Running in debug mode")
