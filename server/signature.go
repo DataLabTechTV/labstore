82e9627e73e1800e06a3259d1475b15b8077e2bf
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-08-04 21:41:46 +0100

    feat: sigv4 complete implementation (no workigng yet)

--- server/signature.go
+++ server/signature.go
@@ -1,35 +1,48 @@
 package server
 
 import (
+	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
-func toCanonicalRequest(r *http.Request, timestamp string) (string, bool) {
-	var canReq strings.Builder
+func buildCanonicalRequest(r *http.Request, signedHeaders []string) (string, bool) {
+	var canonicalRequest strings.Builder
 
-	canReq.WriteString(r.Method)
-	canReq.WriteString("\n")
+	canonicalRequest.WriteString(r.Method)
+	canonicalRequest.WriteString("\n")
 
-	canReq.WriteString(r.URL.Path)
-	canReq.WriteString("\n")
+	canonicalRequest.WriteString(r.URL.Path)
+	canonicalRequest.WriteString("\n")
 
-	canReq.WriteString(r.URL.RawQuery)
-	canReq.WriteString("\n")
+	canonicalRequest.WriteString(r.URL.RawQuery)
+	canonicalRequest.WriteString("\n")
 
-	canReq.WriteString("host:")
-	canReq.WriteString(r.Host)
-	canReq.WriteString("\n")
+	for _, signedHeader := range signedHeaders {
+		header := strings.ToLower(signedHeader)
 
-	canReq.WriteString("x-amz-date:")
-	canReq.WriteString(timestamp)
-	canReq.WriteString("\n\n")
+		if header == "host" {
+			canonicalRequest.WriteString("host:")
+			canonicalRequest.WriteString(r.Host)
+			canonicalRequest.WriteString("\n")
+			continue
+		}
 
-	canReq.WriteString("host;x-amz-date")
-	canReq.WriteString("\n")
+		canonicalRequest.WriteString(header)
+		canonicalRequest.WriteString(":")
+		canonicalRequest.WriteString(strings.TrimSpace(r.Header.Get(signedHeader)))
+		canonicalRequest.WriteString("\n")
+	}
+
+	canonicalRequest.WriteString("\n")
+
+	canonicalRequest.WriteString(strings.Join(signedHeaders, ";"))
+	canonicalRequest.WriteString("\n")
 
 	body, err := io.ReadAll(r.Body)
 
@@ -37,54 +50,168 @@ func toCanonicalRequest(r *http.Request, timestamp string) (string, bool) {
 		return "", false
 	}
 
-	canReq.WriteString(fmt.Sprintf("%x", sha256.Sum256(body)))
+	canonicalRequest.WriteString(fmt.Sprintf("%x", sha256.Sum256(body)))
 
-	return canReq.String(), true
+	return canonicalRequest.String(), true
+}
+
+func buildStringToSign(
+	timestamp string,
+	scope string,
+	canonicalRequest string,
+) string {
+	var stringToSign strings.Builder
+
+	stringToSign.WriteString("AWS4-HMAC-SHA256")
+	stringToSign.WriteString("\n")
+
+	stringToSign.WriteString(timestamp)
+	stringToSign.WriteString("\n")
+
+	stringToSign.WriteString(scope)
+	stringToSign.WriteString("\n")
+
+	stringToSign.WriteString(fmt.Sprintf("%x", sha256.Sum256([]byte(canonicalRequest))))
+	stringToSign.WriteString("\n")
+
+	return stringToSign.String()
+}
+
+func computeSignature(
+	secretKey string,
+	scope string,
+	stringToSign string,
+) (string, bool) {
+	scopeParts := strings.Split(scope, "/")
+	date := scopeParts[0]
+	awsRegion := scopeParts[1]
+	awsService := scopeParts[2]
+
+	hashFunc := sha256.New
+
+	mac := hmac.New(hashFunc, []byte("AWS4"+secretKey))
+	mac.Write([]byte(date))
+	dateKey := mac.Sum(nil)
+
+	mac = hmac.New(hashFunc, dateKey)
+	mac.Write([]byte(awsRegion))
+	dateRegionKey := mac.Sum(nil)
+
+	mac = hmac.New(hashFunc, dateRegionKey)
+	mac.Write([]byte(awsService))
+	dateRegionServiceKey := mac.Sum(nil)
+
+	mac = hmac.New(hashFunc, dateRegionServiceKey)
+	mac.Write([]byte("aws4_request"))
+	signingKey := mac.Sum(nil)
+
+	mac = hmac.New(hashFunc, signingKey)
+	mac.Write([]byte(stringToSign))
+	signature := mac.Sum(nil)
+
+	return string(signature), true
 }
 
 func verifyAWSSigV4(r *http.Request) (string, bool) {
 	auth := r.Header.Get("Authorization")
 
+	log.Debug("Authorization: " + auth)
+
+	// Remove prefix
+
 	if !strings.HasPrefix(auth, "AWS4-HMAC-SHA256") {
 		return "", false
 	}
 
-	var ok bool
-
-	auth, ok = strings.CutPrefix(auth, "AWS4-HMAC-SHA256 ")
+	auth, ok := strings.CutPrefix(auth, "AWS4-HMAC-SHA256 ")
 	if !ok {
 		return "", false
 	}
 
-	// Parse Credential= part
+	// Parse credentials, signed headers, and signature
+
 	parts := strings.Split(auth, ",")
-	var credential string
+
+	var credentials string
+	var signedHeaders []string
+	var signature string
+
 	for _, p := range parts {
 		p = strings.TrimSpace(p)
+
 		if after, ok := strings.CutPrefix(p, "Credential="); ok {
-			credential = after
-			break
+			credentials = after
+		}
+
+		if after, ok := strings.CutPrefix(p, "SignedHeaders="); ok {
+			signedHeaders = strings.Split(after, ";")
+		}
+
+		if after, ok := strings.CutPrefix(p, "Signature="); ok {
+			signature = after
 		}
 	}
-	if credential == "" {
+
+	if credentials == "" {
 		return "", false
 	}
-	accessKey := strings.Split(credential, "/")[0]
-	_, ok = users[accessKey]
+
+	if len(signedHeaders) == 0 {
+		return "", false
+	}
+
+	if signature == "" {
+		return "", false
+	}
+
+	log.Debug("Credentials: " + credentials)
+	log.Debug("SignedHeaders: " + strings.Join(signedHeaders, ";"))
+	log.Debug("Signature: " + signature)
+
+	// Extract access key and scope
+
+	credentialParts := strings.Split(credentials, "/")
+
+	accessKey := credentialParts[0]
+	secretKey, ok := users[accessKey]
+
 	if !ok {
 		return "", false
 	}
 
-	// Here you'd do full signature verification with secretKey, canonical request, etc.
-	// For MVP, we just check presence of accessKey in users.
+	scope := strings.Join(credentialParts[1:], "/")
+
+	log.Debug("Access key: " + accessKey)
+	log.Debug("Scope: " + scope)
+
+	// Compute signature
+
+	canonicalRequest, ok := buildCanonicalRequest(r, signedHeaders)
+
+	if !ok {
+		return "", false
+	}
+
+	log.Debug("Canonical request: " + canonicalRequest)
 
 	timestamp := r.Header.Get("X-Amz-Date")
-	// canonicalRequest, ok := toCanonicalRequest(r, timestamp)
-	_, ok = toCanonicalRequest(r, timestamp)
+	stringToSign := buildStringToSign(timestamp, scope, canonicalRequest)
+
+	log.Debug("String to sign: " + stringToSign)
+
+	recomputedSignature, ok := computeSignature(secretKey, scope, stringToSign)
 
 	if !ok {
 		return "", false
 	}
 
-	return accessKey, true
+	log.Debug("Signature (recomputed): " + fmt.Sprintf("%x", recomputedSignature))
+
+	if signature == recomputedSignature {
+		return accessKey, true
+	}
+
+	log.Error("Original and recomputed signatures differ")
+
+	return "", false
 }
