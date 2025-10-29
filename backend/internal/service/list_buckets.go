8b3335dedec8a768155bb854a2f8fa496ad4cde8
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-10-29 16:51:15 +0000

    refactor(backend): migration to fiber

--- backend/internal/service/list_buckets.go
+++ backend/internal/service/list_buckets.go
@@ -2,57 +2,61 @@ package service
 
 import (
 	"encoding/xml"
-	"net/http"
 	"os"
 	"time"
 
 	"github.com/DataLabTechTV/labstore/backend/internal/config"
 	"github.com/DataLabTechTV/labstore/backend/internal/core"
-	"github.com/DataLabTechTV/labstore/backend/pkg/iam"
+	"github.com/gofiber/fiber/v2"
 )
 
 // !FIXME: move types to a proper location
 
 type Bucket struct {
-	Name         string `xml:"Name"`
-	CreationDate string `xml:"CreationDate"`
+	Name         string
+	CreationDate string
 }
 
 type ListAllMyBucketsResult struct {
 	XMLName xml.Name `xml:"ListAllMyBucketsResult"`
 	Owner   struct {
-		ID          string `xml:"ID"`
-		DisplayName string `xml:"DisplayName"`
+		ID          string
+		DisplayName string
 	}
 	Buckets struct {
-		Bucket []Bucket `xml:"Bucket"`
+		Bucket []Bucket
 	}
 }
 
-// ListBuckets: GET /
-func handleListBuckets(w http.ResponseWriter, _ *http.Request, accessKey string) {
-	if !iam.CheckPolicy(accessKey, "", "ListBuckets") {
-		core.WriteS3Error(w, "AccessDenied", "Access Denied", 403)
-		return
-	}
-
+func ListBuckets(accessKey string) (*ListAllMyBucketsResult, error) {
 	entries, err := os.ReadDir(config.Env.StorageRoot)
 	if err != nil {
-		core.WriteS3Error(w, "InternalError", "Failed to list buckets", 500)
-		return
+		return nil, core.ErrorInternalError("Failed to list buckets")
 	}
 
-	result := ListAllMyBucketsResult{}
-	result.Owner.ID = accessKey
-	result.Owner.DisplayName = accessKey
+	res := ListAllMyBucketsResult{}
+	res.Owner.ID = accessKey
+	res.Owner.DisplayName = accessKey
 
 	for _, e := range entries {
 		if e.IsDir() {
 			b := Bucket{Name: e.Name(), CreationDate: time.Now().Format(time.RFC3339)}
-			result.Buckets.Bucket = append(result.Buckets.Bucket, b)
+			res.Buckets.Bucket = append(res.Buckets.Bucket, b)
 		}
 	}
 
-	w.Header().Set("Content-Type", "application/xml")
-	xml.NewEncoder(w).Encode(result)
+	return &res, nil
+}
+
+// ListBuckets: GET /
+func ListBucketsHandler(c *fiber.Ctx) error {
+	accessKey := c.Params("accessKey")
+
+	res, err := ListBuckets(accessKey)
+	if err != nil {
+		core.HandleError(c, err)
+		return err
+	}
+
+	return c.XML(res)
 }
