00c3d40acf2066d8dbe3f156f98dd1466989f911
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-11-04 13:09:42 +0000

    feat(backend): migration complete, with a few bugs (#12)

--- backend/internal/bucket/head_bucket.go
+++ backend/internal/bucket/head_bucket.go
@@ -1,12 +1,12 @@
 package bucket
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"net/http"
 )
 
-func HeadBucketHandler(c *fiber.Ctx) error {
+func HeadBucketHandler(w http.ResponseWriter, r *http.Request) {
 	// *NOTE: This will likely share code with GET due to using the same headers.
 	// TODO: organize shared code somewhere
 
-	return c.SendStatus(fiber.StatusOK)
+	w.WriteHeader(http.StatusOK)
 }
--- backend/internal/object/error.go
+++ backend/internal/object/error.go
@@ -1,14 +1,15 @@
 package object
 
 import (
+	"net/http"
+
 	"github.com/DataLabTechTV/labstore/backend/internal/core"
-	"github.com/gofiber/fiber/v2"
 )
 
 func ErrorNoSuchKey() *core.S3Error {
 	return &core.S3Error{
 		Code:       "NoSuchKey",
 		Message:    "Object not found",
-		StatusCode: fiber.StatusNotFound,
+		StatusCode: http.StatusNotFound,
 	}
 }
