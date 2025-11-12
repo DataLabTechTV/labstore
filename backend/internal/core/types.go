31cac41e05253f9e0a04af9c069cd7364cfe1b06
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-11-12 18:56:16 +0000

    feat(backend): basic ListObjects (V1) implementation (#21)

---
+++ backend/internal/core/types.go
@@ -0,0 +1,20 @@
+package core
+
+import (
+	"encoding/xml"
+	"time"
+)
+
+const ISO8601 = "2006-01-02T15:04:05Z"
+
+type Timestamp time.Time
+
+func (t Timestamp) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	formatted := time.Time(t).Format(ISO8601)
+	return e.EncodeElement(formatted, start)
+}
+
+type Owner struct {
+	ID          string
+	DisplayName string // deprecated, but we'll support it
+}
