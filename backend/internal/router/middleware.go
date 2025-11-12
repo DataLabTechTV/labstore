501b579a912b9a5016e6b11bf3a3f2cc0309d2dd
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-11-12 12:00:42 +0000

    refactor(backend): clean up router, splitting middleware logic and reorganizing routes to match docs (#31)

---
+++ backend/internal/router/middleware.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+type Middleware func(http.Handler) http.Handler
+
+func (Middleware) Stack(mw ...Middleware) Middleware {
+	slog.Debug("Stacking middleware")
+
+	return func(next http.Handler) http.Handler {
+		for i := len(mw) - 1; i >= 0; i-- {
+			next = mw[i](next)
+		}
+
+		return next
+	}
+}
