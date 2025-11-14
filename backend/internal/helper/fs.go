889b112c2cfca04d2f43960075f5f3a411be70dd
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-11-14 11:58:21 +0000

    fix(backend): listing wasn't respecting prefix or parsing delimiter (#21)

--- backend/internal/helper/fs.go
+++ backend/internal/helper/fs.go
@@ -6,3 +6,12 @@ func FileExists(path string) bool {
 	_, err := os.Stat(path)
 	return !os.IsNotExist(err)
 }
+
+func IsDir(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return info.IsDir()
+}
