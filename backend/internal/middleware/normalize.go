8c88917ff5b8dc81cc3aa778eff8e5387a3e080f
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-11-28 18:25:54 +0000

    refactor(backend): replace TrimRight wiht TrimSuffix for correctness (#52)

--- backend/internal/middleware/normalize.go
+++ backend/internal/middleware/normalize.go
@@ -8,7 +8,7 @@ import (
 func NormalizeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimRight(r.URL.Path, "/")
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
 		}
 		next.ServeHTTP(w, r)
 	})
