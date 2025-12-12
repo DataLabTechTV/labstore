e06e5b8c555a5e294042cc9bb3a27db8c20e4554
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-12-12 16:43:09 +0000

    feat(backend): add constants for all iam op to implement (#82)

--- backend/internal/iam/ops.go
+++ backend/internal/iam/ops.go
@@ -2,17 +2,68 @@ package iam
 
 type IAMOp string
 
-// TODO: remaining ops for CRUD
 const (
-	// Users
-	OpCreateUser      IAMOp = "CreateUser"
-	OpCreateAccessKey IAMOp = "CreateAccessKey"
+	// ======================
+	// USERS
+	// ======================
 
-	// Groups
-	OpCreateGroup IAMOp = "CreateGroup"
+	// --- Users: Create ---
+	OpCreateUser       IAMOp = "CreateUser"
+	OpCreateAccessKey  IAMOp = "CreateAccessKey"
+	OpAttachUserPolicy IAMOp = "AttachUserPolicy"
 
-	// Policies
-	OpCreatePolicy      IAMOp = "CreatePolicy"
-	OpAttachUserPolicy  IAMOp = "AttachUserPolicy"
+	// --- Users: Read ---
+	OpGetUser                  IAMOp = "GetUser"
+	OpListAccessKeys           IAMOp = "ListAccessKeys"
+	OpListAttachedUserPolicies IAMOp = "ListAttachedUserPolicies"
+	OpGetUserPolicy            IAMOp = "GetUserPolicy"
+
+	// --- Users: Update ---
+	OpPutUserPolicy IAMOp = "PutUserPolicy"
+
+	// --- Users: Delete ---
+	OpDeleteUser       IAMOp = "DeleteUser"
+	OpDeleteAccessKey  IAMOp = "DeleteAccessKey"
+	OpDetachUserPolicy IAMOp = "DetachUserPolicy"
+
+	// ======================
+	// GROUPS
+	// ======================
+
+	// --- Groups: Create ---
+	OpCreateGroup       IAMOp = "CreateGroup"
+	OpAddUserToGroup    IAMOp = "AddUserToGroup"
 	OpAttachGroupPolicy IAMOp = "AttachGroupPolicy"
+
+	// --- Groups: Read ---
+	OpGetGroup                  IAMOp = "GetGroup"
+	OpListAttachedGroupPolicies IAMOp = "ListAttachedGroupPolicies"
+	OpGetGroupPolicy            IAMOp = "GetGroupPolicy"
+
+	// --- Groups: Update ---
+	OpPutGroupPolicy IAMOp = "PutGroupPolicy"
+
+	// --- Groups: Delete ---
+	OpDeleteGroup         IAMOp = "DeleteGroup"
+	OpRemoveUserFromGroup IAMOp = "RemoveUserFromGroup"
+	OpDetachGroupPolicy   IAMOp = "DetachGroupPolicy"
+
+	// ======================
+	// POLICIES
+	// ======================
+
+	// --- Policies: Create ---
+	OpCreatePolicy        IAMOp = "CreatePolicy"
+	OpCreatePolicyVersion IAMOp = "CreatePolicyVersion"
+
+	// --- Policies: Read ---
+	OpGetPolicy        IAMOp = "GetPolicy"
+	OpGetPolicyVersion IAMOp = "GetPolicyVersion"
+
+	// --- Policies: Update ---
+	// N/A
+
+	// --- Policies: Delete ---
+	OpDeletePolicy        IAMOp = "DeletePolicy"
+	OpDeletePolicyVersion IAMOp = "DeletePolicyVersion"
 )
