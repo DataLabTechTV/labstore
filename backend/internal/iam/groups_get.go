8858f1724dcaca1a72879631ab4f536a8b10b0d4
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-12-17 11:42:16 +0000

    feat(backend): get group (#82)

--- backend/internal/iam/groups_get.go
+++ backend/internal/iam/groups_get.go
@@ -1,10 +1,30 @@
 package iam
 
 import (
+	"encoding/xml"
 	"log/slog"
 	"net/http"
 	"time"
+
+	"github.com/IllumiKnowLabs/labstore/backend/internal/core"
+	"github.com/IllumiKnowLabs/labstore/backend/internal/errs"
 )
+
+type GetGroupResponse struct {
+	XMLName          xml.Name `xml:"https://iam.amazonaws.com/doc/2010-05-08/ GetGroupResponse"`
+	GetGroupResult   *GetGroupResult
+	ResponseMetadata *ResponseMetadata
+}
+
+type GetGroupResult struct {
+	Group       *GroupResult
+	Users       *UserMembers
+	IsTruncated bool
+}
+
+type UserMembers struct {
+	Member []*UserResult `xml:"member"`
+}
 
 func (store *Store) GetGroupByName(name string) (*Group, error) {
 	var group Group
@@ -14,6 +34,7 @@ func (store *Store) GetGroupByName(name string) (*Group, error) {
 		return nil, err
 	}
 
+	// Load policies
 	policies, err := store.getPoliciesByEntityID(ArnGroup, group.GroupID)
 	if err != nil {
 		return nil, err
@@ -21,6 +42,16 @@ func (store *Store) GetGroupByName(name string) (*Group, error) {
 	group.PolicyIDs = make([]string, len(policies))
 	for i, policy := range policies {
 		group.PolicyIDs[i] = policy.PolicyID
+	}
+
+	// Load users
+	users, err := store.getUsersByGroupID(group.GroupID)
+	if err != nil {
+		return nil, err
+	}
+	group.UserIDs = make([]string, len(users))
+	for i, user := range users {
+		group.UserIDs[i] = user.UserID
 	}
 
 	store.CachedGroups[group.Name] = &CachedGroup{
@@ -32,5 +63,41 @@ func (store *Store) GetGroupByName(name string) (*Group, error) {
 }
 
 func GetGroupHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	groupName := r.URL.Query().Get("GroupName")
+	if groupName == "" {
+		errs.Handle(w, errs.HTTPMissingQueryParam("GroupName"))
+		return
+	}
+
+	group, err := store.GetGroupByName(groupName)
+	if err != nil {
+		errs.Handle(w, errs.IAMNoSuchEntity(string(errs.ErrEntityTypeGroup), groupName))
+		return
+	}
+
+	users, err := store.getUsersByGroupID(group.GroupID)
+	if err != nil {
+		errs.Handle(w, errs.IAMServiceFailure())
+		return
+	}
+
+	userResults := make([]*UserResult, len(users))
+	for i, user := range users {
+		userResults[i] = user.Result()
+	}
+
+	response := &GetGroupResponse{
+		GetGroupResult: &GetGroupResult{
+			Group: group.Result(),
+			Users: &UserMembers{
+				Member: userResults,
+			},
+			IsTruncated: false,
+		},
+		ResponseMetadata: &ResponseMetadata{
+			RequestId: core.NewRequestID(),
+		},
+	}
+
+	core.WriteXML(w, http.StatusOK, response)
 }
