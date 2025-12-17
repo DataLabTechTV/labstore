34fbeb3fad0e88b2e85fccf25ed69c60771974d2
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-12-17 14:59:21 +0000

    feat(backend): list attached user/group policies (#82)

--- backend/internal/iam/policies_list.go
+++ backend/internal/iam/policies_list.go
@@ -3,6 +3,9 @@ package iam
 import (
 	"encoding/xml"
 	"net/http"
+
+	"github.com/IllumiKnowLabs/labstore/backend/internal/core"
+	"github.com/IllumiKnowLabs/labstore/backend/internal/errs"
 )
 
 type ListAttachedUserPoliciesResponse struct {
@@ -16,8 +19,19 @@ type ListAttachedUserPoliciesResult struct {
 	IsTruncated      bool
 }
 
+type ListAttachedGroupPoliciesResponse struct {
+	XMLName                         xml.Name `xml:"https://iam.amazonaws.com/doc/2010-05-08/ ListAttachedGroupPoliciesResponse"`
+	ListAttachedGroupPoliciesResult *ListAttachedGroupPoliciesResult
+	ResponseMetadata                *ResponseMetadata
+}
+
+type ListAttachedGroupPoliciesResult struct {
+	AttachedPolicies *AttachedPolicies
+	IsTruncated      bool
+}
+
 type AttachedPolicies struct {
-	Member *AttachedPoliciesMember `xml:"member"`
+	Member []*AttachedPoliciesMember `xml:"member"`
 }
 
 type AttachedPoliciesMember struct {
@@ -26,9 +40,85 @@ type AttachedPoliciesMember struct {
 }
 
 func ListAttachedUserPoliciesHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	userName := r.URL.Query().Get("UserName")
+	if userName == "" {
+		errs.Handle(w, errs.HTTPMissingQueryParam("UserName"))
+		return
+	}
+
+	user, err := store.GetUserByName(userName)
+	if err != nil {
+		errs.Handle(w, errs.IAMNoSuchEntity(string(errs.ErrEntityTypeUser), userName))
+		return
+	}
+
+	members := make([]*AttachedPoliciesMember, len(user.PolicyIDs))
+	for i, policyID := range user.PolicyIDs {
+		policy, err := store.GetPolicyByID(policyID)
+		if err != nil {
+			errs.Handle(w, errs.IAMServiceFailure())
+			return
+		}
+
+		members[i] = &AttachedPoliciesMember{
+			PolicyName: policy.Name,
+			PolicyArn:  policy.Arn,
+		}
+	}
+
+	response := &ListAttachedUserPoliciesResponse{
+		ListAttachedUserPoliciesResult: &ListAttachedUserPoliciesResult{
+			AttachedPolicies: &AttachedPolicies{
+				Member: members,
+			},
+			IsTruncated: false,
+		},
+		ResponseMetadata: &ResponseMetadata{
+			RequestId: core.NewRequestID(),
+		},
+	}
+
+	core.WriteXML(w, http.StatusOK, response)
 }
 
 func ListAttachedGroupPoliciesHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	groupName := r.URL.Query().Get("GroupName")
+	if groupName == "" {
+		errs.Handle(w, errs.HTTPMissingQueryParam("GroupName"))
+		return
+	}
+
+	group, err := store.GetGroupByName(groupName)
+	if err != nil {
+		errs.Handle(w, errs.IAMNoSuchEntity(string(errs.ErrEntityTypeGroup), groupName))
+		return
+	}
+
+	members := make([]*AttachedPoliciesMember, len(group.PolicyIDs))
+	for i, policyID := range group.PolicyIDs {
+		policy, err := store.GetPolicyByID(policyID)
+		if err != nil {
+			errs.Handle(w, errs.IAMServiceFailure())
+			return
+		}
+
+		members[i] = &AttachedPoliciesMember{
+			PolicyName: policy.Name,
+			PolicyArn:  policy.Arn,
+		}
+	}
+
+	response := &ListAttachedGroupPoliciesResponse{
+		ListAttachedGroupPoliciesResult: &ListAttachedGroupPoliciesResult{
+			AttachedPolicies: &AttachedPolicies{
+				Member: members,
+			},
+			IsTruncated: false,
+		},
+		ResponseMetadata: &ResponseMetadata{
+			RequestId: core.NewRequestID(),
+		},
+	}
+
+	core.WriteXML(w, http.StatusOK, response)
 }
