6989397e1fd2e4780898d9a03e38987b359b6585
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-12-17 12:51:44 +0000

    feat(backend): remove user from group (#82)

--- backend/internal/iam/groups_remove_user.go
+++ backend/internal/iam/groups_remove_user.go
@@ -1,7 +1,108 @@
 package iam
 
-import "net/http"
+import (
+	"encoding/xml"
+	"errors"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/IllumiKnowLabs/labstore/backend/internal/core"
+	"github.com/IllumiKnowLabs/labstore/backend/internal/errs"
+)
+
+type RemoveUserFromGroupResponse struct {
+	XMLName          xml.Name `xml:"https://iam.amazonaws.com/doc/2010-05-08/ RemoveUserFromGroupResponse"`
+	ResponseMetadata *ResponseMetadata
+}
+
+func (store *Store) RemoveUserFromGroup(userName, groupName string) error {
+	group, err := store.GetGroupByName(groupName)
+	if err != nil {
+		slog.Error("remove user from group", "err", err)
+		return &errs.ErrNotFound{Type: errs.ErrEntityTypeGroup, Resource: groupName}
+	}
+
+	user, err := store.GetUserByName(userName)
+	if err != nil {
+		slog.Error("remove user from group", "err", err)
+		return &errs.ErrNotFound{Type: errs.ErrEntityTypeUser, Resource: userName}
+	}
+
+	query := `
+	DELETE FROM group_users
+	WHERE group_id = $1
+	AND user_id = $2
+	`
+
+	res, err := store.writeDB.Exec(query, group.GroupID, user.UserID)
+	if err != nil {
+		slog.Error("remove user from group", "err", err)
+		return err
+	}
+
+	n, err := res.RowsAffected()
+	if err != nil {
+		slog.Error("remove user from group", "err", err)
+		return err
+	}
+	if n < 1 {
+		slog.Error("remove user from group: 0 deleted")
+		return &errs.ErrUserNotInGroup{UserID: user.UserID, GroupID: group.GroupID}
+	}
+
+	group, err = store.GetGroupByName(groupName)
+	if err != nil {
+		slog.Error("remove user from group", "err", err)
+		return &errs.ErrNotFound{Type: errs.ErrEntityTypeGroup, Resource: groupName}
+	}
+
+	if _, ok := store.CachedGroups[group.GroupID]; ok {
+		store.CachedGroups[group.GroupID].Group = group
+		store.CachedGroups[group.GroupID].LoadedAt = time.Now()
+	}
+
+	return nil
+}
 
 func RemoveUserFromGroupHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	groupName := r.URL.Query().Get("GroupName")
+	if groupName == "" {
+		errs.Handle(w, errs.HTTPMissingQueryParam("GroupName"))
+		return
+	}
+
+	userName := r.URL.Query().Get("UserName")
+	if groupName == "" {
+		errs.Handle(w, errs.HTTPMissingQueryParam("UserName"))
+		return
+	}
+
+	if err := store.RemoveUserFromGroup(userName, groupName); err != nil {
+		var errNotFound *errs.ErrNotFound
+		if errors.As(err, &errNotFound) {
+			errs.Handle(w, errs.IAMNoSuchEntity(string(errNotFound.Type), errNotFound.Resource))
+			return
+		}
+
+		var errUserNotInGroup *errs.ErrUserNotInGroup
+		if errors.As(err, &errUserNotInGroup) {
+			errs.Handle(w, errs.IAMNoSuchEntity(
+				errs.ErrEntityTypeUserGroup,
+				errUserNotInGroup.Description(),
+			))
+			return
+		}
+
+		errs.Handle(w, errs.IAMServiceFailure())
+		return
+	}
+
+	response := &RemoveUserFromGroupResponse{
+		ResponseMetadata: &ResponseMetadata{
+			RequestId: core.NewRequestID(),
+		},
+	}
+
+	core.WriteXML(w, http.StatusOK, response)
 }
