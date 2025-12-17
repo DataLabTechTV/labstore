07c7bdf8abd0903e747a9eb182e4db12ca94f01f
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-12-17 14:33:38 +0000

    refactor(backend): move user get methods to the contextually appropriate file (#82)

--- backend/internal/iam/users_get.go
+++ backend/internal/iam/users_get.go
@@ -58,6 +58,66 @@ func (store *Store) GetUserByName(name string) (*User, error) {
 	return &user, nil
 }
 
+func (store *Store) GetUserByAccessKey(accessKey string) (*User, error) {
+	if cachedUser, ok := store.CachedUsers[accessKey]; ok {
+		if cachedUser.NeverExpire || time.Since(cachedUser.LoadedAt) < store.ttl {
+			return cachedUser.User, nil
+		}
+
+		slog.Debug("invalidating cached user", "accessKey", accessKey)
+		delete(store.CachedUsers, accessKey)
+	}
+
+	var user User
+	if err := store.readDB.Get(&user, `SELECT * FROM users WHERE access_key = $1`, accessKey); err != nil {
+		return nil, err
+	}
+
+	// Load policies
+	policies, err := store.getPoliciesByEntityID(ArnUser, user.UserID)
+	if err != nil {
+		return nil, err
+	}
+	user.PolicyIDs = make([]string, len(policies))
+	for i, policy := range policies {
+		user.PolicyIDs[i] = policy.PolicyID
+	}
+
+	// Load groups
+	groups, err := store.getGroupsByUserID(user.UserID)
+	if err != nil {
+		return nil, err
+	}
+	user.GroupIDs = make([]string, len(groups))
+	for i, group := range groups {
+		user.GroupIDs[i] = group.GroupID
+	}
+
+	store.CachedUsers[user.AccessKeyID.String] = &CachedUser{
+		User:     &user,
+		LoadedAt: time.Now(),
+	}
+
+	return &user, nil
+}
+
+func (store *Store) getUsersByGroupID(groupID string) ([]*User, error) {
+	var users []*User
+
+	query := `
+	SELECT * FROM users WHERE user_id = (
+		SELECT user_id FROM group_users WHERE group_id = $1
+	)
+	`
+
+	if err := store.readDB.Select(&users, query, groupID); err != nil {
+		slog.Error("get groups by user id", "err", err)
+		return nil, err
+	}
+
+	return users, nil
+}
+
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	userName := r.URL.Query().Get("UserName")
 	if userName == "" {
