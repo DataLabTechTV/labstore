00d6705d8550799a6a36f532e007f4e25071706c
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-12-17 12:10:00 +0000

    feat(backend): delete group (#82)

--- backend/internal/iam/groups_delete.go
+++ backend/internal/iam/groups_delete.go
@@ -1,7 +1,66 @@
 package iam
 
-import "net/http"
+import (
+	"encoding/xml"
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/IllumiKnowLabs/labstore/backend/internal/core"
+	"github.com/IllumiKnowLabs/labstore/backend/internal/errs"
+)
+
+type DeleteGroupResponse struct {
+	XMLName          xml.Name `xml:"https://iam.amazonaws.com/doc/2010-05-08/ DeleteGroupResponse"`
+	ResponseMetadata *ResponseMetadata
+}
+
+func (store *Store) DeleteGroup(name string) error {
+	group, err := store.GetGroupByName(name)
+	if err != nil {
+		slog.Error("delete group", "err", err)
+		return &errs.ErrNotFound{Type: errs.ErrEntityTypeGroup, Resource: name}
+	}
+
+	query := `
+	DELETE FROM groups
+	WHERE name = $1
+	`
+
+	_, err = store.writeDB.Exec(query, group.Name)
+	if err != nil {
+		slog.Error("delete group", "err", err)
+		return err
+	}
+
+	delete(store.CachedGroups, group.GroupID)
+
+	return nil
+}
 
 func DeleteGroupHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	groupName := r.URL.Query().Get("GroupName")
+	if groupName == "" {
+		errs.Handle(w, errs.HTTPMissingQueryParam("GroupName"))
+		return
+	}
+
+	if err := store.DeleteGroup(groupName); err != nil {
+		var errNotFound *errs.ErrNotFound
+		if errors.As(err, &errNotFound) {
+			errs.Handle(w, errs.IAMNoSuchEntity(string(errNotFound.Type), errNotFound.Resource))
+			return
+		}
+
+		errs.Handle(w, errs.IAMServiceFailure())
+		return
+	}
+
+	response := &DeleteGroupResponse{
+		ResponseMetadata: &ResponseMetadata{
+			RequestId: core.NewRequestID(),
+		},
+	}
+
+	core.WriteXML(w, http.StatusOK, response)
 }
