c172581d35dd09c161f91996ebc3ae45c3a511b2
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-12-31 12:20:14 +0000

    chore(cli): extend scaffolding with entrypoint commands for s3, iam, and admin (#99)

--- cli/cmd/labstore/root.go
+++ cli/cmd/labstore/root.go
@@ -2,26 +2,41 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/IllumiKnowLabs/labstore/backend/pkg/config"
+	"github.com/IllumiKnowLabs/labstore/backend/pkg/constants"
+	"github.com/IllumiKnowLabs/labstore/backend/pkg/helper"
+	"github.com/IllumiKnowLabs/labstore/backend/pkg/logger"
 	"github.com/spf13/cobra"
 )
 
 func NewRootCmd() *cobra.Command {
 	var cmd = &cobra.Command{
-		// Use:   fmt.Sprintf("%s", strings.ToLower(constants.Name)),
-		// Short: fmt.Sprintf("%s, by %s", constants.Name, constants.Author),
-		// Long:  fmt.Sprintf("%s - %s, by %s", constants.Name, constants.Description, constants.Author),
+		Use:   fmt.Sprintf("%s", strings.ToLower(constants.Name)),
+		Short: fmt.Sprintf("%s, by %s", constants.Name, constants.Author),
+		Long:  fmt.Sprintf("%s - %s, by %s", constants.Name, constants.Description, constants.Author),
 
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			// TODO
-			fmt.Println("TODO")
+			welcomeMsg := fmt.Sprintf("🚀 Welcome to %s, by %s", constants.Name, constants.Author)
+			if helper.SupportsBox() {
+				helper.Box(welcomeMsg)
+			} else {
+				fmt.Println(welcomeMsg)
+			}
+
+			debug := helper.Must(cmd.Flags().GetBool("debug"))
+			logger.Init(logger.WithDebugFlag(debug))
+
+			config.Load(cmd)
 		},
 	}
 
 	cmd.PersistentFlags().Bool("debug", false, "Set debug level for logging")
-	cmd.PersistentFlags().Bool("pprof", false, "Enable profiler")
-	cmd.PersistentFlags().String("pprof-host", "localhost", "Profiler host")
-	cmd.PersistentFlags().Int("pprof-port", 6060, "Profiler port")
+
+	cmd.AddCommand(NewS3Cmd())
+	cmd.AddCommand(NewIAMCmd())
+	cmd.AddCommand(NewAdminCmd())
 
 	return cmd
 }
