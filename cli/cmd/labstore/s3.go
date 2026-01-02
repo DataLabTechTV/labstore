dd8c1ef0f5a35762220473c05c850d17b8b419f8
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-02 17:22:11 +0000

    feat(cli): integrate profile loader, s3 client, and s3 handlers (#99)

--- cli/cmd/labstore/s3.go
+++ cli/cmd/labstore/s3.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/IllumiKnowLabs/labstore/backend/pkg/config"
+	"github.com/IllumiKnowLabs/labstore/backend/pkg/helper"
+	"github.com/IllumiKnowLabs/labstore/cli/internal/credentials"
+	"github.com/IllumiKnowLabs/labstore/cli/internal/handlers"
 	"github.com/IllumiKnowLabs/labstore/client/s3"
 	"github.com/spf13/cobra"
 )
@@ -11,13 +17,44 @@ func NewS3Cmd() *cobra.Command {
 		Use:   "s3",
 		Short: "S3 client",
 		Run: func(cmd *cobra.Command, args []string) {
-			s3.Init()
+			credentials.Init()
+
+			profileName := helper.Must(cmd.Flags().GetString("profile"))
+			slog.Debug("s3", "profile", profileName, "host", config.S3.Server.Host, "port", config.S3.Server.Port)
+
+			var profile *credentials.Profile
+			var err error
+			if profileName == "" {
+				profile, err = credentials.LoadDefaultProfile()
+			} else {
+				profile, err = credentials.LoadProfile(profileName)
+			}
+
+			if err != nil {
+				slog.Error(err.Error())
+				os.Exit(1)
+			}
+
+			client := s3.NewS3Client(
+				config.S3.Server.Host,
+				config.S3.Server.Port,
+				profile.AccessKey,
+				profile.SecretKey,
+			)
+
+			handler := handlers.NewS3Handler(client)
+
+			switch {
+			case args[0] == "ls":
+				handler.ListBuckets()
+
+			case args[0] == "ls" && len(args) >= 2:
+				handler.ListObjects(args[1])
+			}
 		},
 	}
 
-	// TODO: need a way to manage credentials for other users (local config)
-	cmd.Flags().String("admin-auth-access-key", config.DefaultAdminAuthAccessKey, "Administrator account access key")
-	cmd.Flags().String("admin-auth-secret-key", config.DefaultAdminSecretKey, "Administrator account secret key")
+	cmd.Flags().String("profile", "", "Profile used for authentication")
 
 	cmd.Flags().String("s3-server-host", config.DefaultS3ServerHost, "Listening host for S3-compatible server")
 	cmd.Flags().Uint16("s3-server-port", config.DefaultS3ServerPort, "Listening port for S3-compatible server")
