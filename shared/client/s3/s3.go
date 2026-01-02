898d42922717f7ca1a67be1eb1c6173b63f604ff
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-02 17:12:13 +0000

    feat(client): s3 client type and constructor (#99)

--- shared/client/s3/s3.go
+++ shared/client/s3/s3.go
@@ -1,10 +1,27 @@
 package s3
 
-func Init() {
-	// TODO: DELETE PLACEHOLDER
+type S3Client struct {
+	Host      string
+	Port      uint16
+	AccessKey string
+	SecretKey string
 }
 
-func List(path string) ([]string, error) {
+func NewS3Client(host string, port uint16, accessKey, secretKey string) *S3Client {
+	return &S3Client{
+		Host:      host,
+		Port:      port,
+		AccessKey: accessKey,
+		SecretKey: secretKey,
+	}
+}
+
+func (client *S3Client) ListBuckets() ([]string, error) {
+	// TODO: implement
+	return []string{}, nil
+}
+
+func (client *S3Client) ListObjects(bucket, key string) ([]string, error) {
 	// TODO: implement
 	return []string{}, nil
 }
