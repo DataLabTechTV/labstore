212be1882a28a4d526141605ca93b017933099a2
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-04 17:39:57 +0000

    feat(cli): update bucket view design (#99)

--- cli/internal/tui/static.go
+++ cli/internal/tui/static.go
@@ -3,7 +3,20 @@ package tui
 import (
 	"fmt"
 
+	t "github.com/IllumiKnowLabs/labstore/backend/pkg/types"
 	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	DateStyle = lipgloss.NewStyle().
+			Width(25).
+			Align(lipgloss.Left).
+			Foreground(ActivePalette.TextMuted)
+
+	PathStyle = lipgloss.NewStyle().
+			Width(60).
+			Align(lipgloss.Left).
+			Foreground(ActivePalette.TextPrimary)
 )
 
 func PrintError(err error) {
@@ -12,12 +25,24 @@ func PrintError(err error) {
 	fmt.Println(err.Error())
 }
 
-func PrintFileList(paths []string) {
-	fileStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("201")).
-		Bold(false)
+func PrintBuckets(buckets []t.Bucket) {
 
-	for _, path := range paths {
-		fmt.Println(fileStyle.Render(path))
+	for _, bucket := range buckets {
+		date := fmt.Sprintf("[%s]", bucket.CreationDate)
+		bucketView := lipgloss.JoinHorizontal(
+			lipgloss.Top,
+			DateStyle.Render(date),
+			PathStyle.Render(bucket.Name+"/"),
+		)
+
+		fmt.Println(bucketView)
+	}
+}
+
+func PrintObjects(bucket string, objects []t.Object) {
+	for _, object := range objects {
+		path := fmt.Sprintf("%s/%s", bucket, object.Key)
+		objectView := PathStyle.Render(path)
+		fmt.Println(objectView)
 	}
 }
