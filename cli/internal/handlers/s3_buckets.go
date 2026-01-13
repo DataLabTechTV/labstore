cfbc52daffa7f202703670ecba903ed2858090bf
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-13 15:34:46 +0000

    refactor(cli): split s3 handlers into multiple files (#99)

---
+++ cli/internal/handlers/s3_buckets.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"github.com/IllumiKnowLabs/labstore/cli/internal/tui"
+)
+
+func (h *S3Handler) ListBuckets() {
+	tui.PrintTitle("ListBuckets")
+
+	buckets, err := h.Client.ListBuckets()
+	if err != nil {
+		tui.PrintError(err)
+		return
+	}
+
+	for _, bucket := range buckets {
+		tui.PrintBucket(bucket)
+	}
+}
