b17b53aad43635688903679454aafbc06cc71685
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-13 16:17:41 +0000

    refactor(cli): split s3 handlers into multiple files (#99)

--- cli/internal/handlers/s3_buckets.go
+++ cli/internal/handlers/s3_buckets.go
@@ -1,19 +1,31 @@
 package handlers
 
 import (
+	"fmt"
+
+	"github.com/IllumiKnowLabs/labstore/backend/pkg/helper"
 	"github.com/IllumiKnowLabs/labstore/cli/internal/tui"
 )
 
-func (h *S3Handler) ListBuckets() {
-	tui.PrintTitle("ListBuckets")
-
-	buckets, err := h.Client.ListBuckets()
-	if err != nil {
-		tui.PrintError(err)
-		return
+func (h *S3Handler) ListObjects(bucket string, key *string) {
+	if key == nil {
+		key = helper.StringPtr("/")
 	}
 
-	for _, bucket := range buckets {
-		tui.PrintBucket(bucket)
+	tui.PrintTitle(fmt.Sprintf("ListObjectsV2: %s", bucket))
+
+	res := h.Client.ListObjects(bucket, *key, true)
+	for r := range res {
+		if r.Err != nil {
+			tui.PrintError(r.Err)
+			return
+		}
+
+		switch {
+		case r.IsObject():
+			tui.PrintObject(*r.Object)
+		case r.IsCommonPrefix():
+			tui.PrintCommonPrefix(*r.CommonPrefix)
+		}
 	}
 }
