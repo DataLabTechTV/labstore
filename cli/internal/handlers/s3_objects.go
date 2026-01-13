a1804825b05a4323c6bd0fedfdee7620cc9c7b62
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-13 16:17:41 +0000

    refactor(cli): split s3 handlers into multiple files (#99)

--- cli/internal/handlers/s3_objects.go
+++ cli/internal/handlers/s3_objects.go
@@ -8,29 +8,6 @@ import (
 	"github.com/IllumiKnowLabs/labstore/cli/internal/tui"
 )
 
-func (h *S3Handler) ListObjects(bucket string, key *string) {
-	if key == nil {
-		key = helper.StringPtr("/")
-	}
-
-	tui.PrintTitle(fmt.Sprintf("ListObjectsV2: %s", bucket))
-
-	res := h.Client.ListObjects(bucket, *key, true)
-	for r := range res {
-		if r.Err != nil {
-			tui.PrintError(r.Err)
-			return
-		}
-
-		switch {
-		case r.IsObject():
-			tui.PrintObject(*r.Object)
-		case r.IsCommonPrefix():
-			tui.PrintCommonPrefix(*r.CommonPrefix)
-		}
-	}
-}
-
 func (h *S3Handler) PutObject(bucket, key, localPath string) {
 	if !helper.FileExists(localPath) {
 		tui.PrintError(fmt.Errorf("file not found: %s", localPath))
