26c5dce4fb48f1aac5f357491b1d33d61f8b01af
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-13 15:34:46 +0000

    refactor(cli): split s3 handlers into multiple files (#99)

---
+++ cli/internal/handlers/s3_objects.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/IllumiKnowLabs/labstore/backend/pkg/helper"
+	"github.com/IllumiKnowLabs/labstore/cli/internal/tui"
+)
+
+func (h *S3Handler) ListObjects(bucket string, key *string) {
+	if key == nil {
+		key = helper.StringPtr("/")
+	}
+
+	tui.PrintTitle(fmt.Sprintf("ListObjectsV2: %s", bucket))
+
+	res := h.Client.ListObjects(bucket, *key, true)
+	for r := range res {
+		if r.Err != nil {
+			tui.PrintError(r.Err)
+			return
+		}
+
+		switch {
+		case r.IsObject():
+			tui.PrintObject(*r.Object)
+		case r.IsCommonPrefix():
+			tui.PrintCommonPrefix(*r.CommonPrefix)
+		}
+	}
+}
+
+func (h *S3Handler) PutObject(bucket, key, localPath string) {
+	if !helper.FileExists(localPath) {
+		tui.PrintError(fmt.Errorf("file not found: %s", localPath))
+		return
+	}
+
+	tui.PrintTitle(fmt.Sprintf("PutObject: %s/%s", bucket, key))
+
+	file, err := os.Open(localPath)
+	if err != nil {
+		tui.PrintError(err)
+		return
+	}
+
+	progressBar, err := tui.NewProgressBarModel()
+	go progressBar.Run()
+	defer progressBar.Close()
+
+	err = h.Client.PutObject(bucket, key, file, progressBar.Progress)
+	if err != nil {
+		tui.PrintError(err)
+		return
+	}
+
+	tui.PrintSuccess(fmt.Sprintf("%s uploaded", localPath))
+}
