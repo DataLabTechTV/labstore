d6aaff5190748820b9cf9ec9b55e2614b18f82cf
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-13 20:07:52 +0000

    feat(client): head object (#99)

--- shared/client/s3/objects.go
+++ shared/client/s3/objects.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/IllumiKnowLabs/labstore/backend/pkg/errs"
 	"github.com/IllumiKnowLabs/labstore/backend/pkg/helper"
 	"github.com/IllumiKnowLabs/labstore/client"
 )
+
+type HeadObjectResponse struct {
+	ContentType   string
+	ContentLength int64
+	LastModified  time.Time
+	StatusCode    int
+}
 
 func (client *S3Client) PutObject(bucket, key string, file *os.File, progress chan<- client.Progress) (int, error) {
 	reqURL, err := client.baseURL.Parse(fmt.Sprintf("%s/%s", bucket, key))
@@ -40,4 +49,45 @@ func (client *S3Client) PutObject(bucket, key string, file *os.File, progress ch
 
 	return 0, &s3Err
 
+}
+
+func (client *S3Client) HeadObject(bucket, key string) (*HeadObjectResponse, error) {
+	reqURL, err := client.baseURL.Parse(fmt.Sprintf("%s/%s", bucket, key))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.DoSigV4Request("HEAD", reqURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	defer helper.CloseWithErr(resp.Body, &err)
+
+	contentLength, err := strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	lastModified, err := time.Parse(http.TimeFormat, resp.Header.Get("Last-Modified"))
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		out := &HeadObjectResponse{
+			ContentType:   resp.Header.Get("Content-Type"),
+			ContentLength: contentLength,
+			LastModified:  lastModified,
+			StatusCode:    resp.StatusCode,
+		}
+		return out, nil
+	}
+
+	var s3Err errs.S3Error
+	if err := helper.ReadXML(resp.Body, &s3Err); err != nil {
+		return nil, err
+	}
+	s3Err.StatusCode = resp.StatusCode
+
+	return nil, &s3Err
 }
