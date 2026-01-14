7705156f6e2427ab871154f676b2c231b294fb50
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-14 09:22:01 +0000

    feat(cli): head object, metadata rendering (#99)

--- cli/internal/render/meta.go
+++ cli/internal/render/meta.go
@@ -1,5 +1,12 @@
 package render
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/IllumiKnowLabs/labstore/cli/internal/format"
+)
+
 type MetaType int
 
 const (
@@ -11,4 +18,29 @@ const (
 type Meta struct {
 	Type  MetaType
 	Value any
+}
+
+func NewMetaString(str string) Meta {
+	return Meta{Type: MetaTypeString, Value: str}
+}
+
+func NewMetaDate(date time.Time) Meta {
+	return Meta{Type: MetaTypeDate, Value: date}
+}
+
+func NewMetaSize(size int64) Meta {
+	return Meta{Type: MetaTypeSize, Value: size}
+}
+
+func (m Meta) Render() string {
+	switch m.Type {
+	case MetaTypeString:
+		return m.Value.(string)
+	case MetaTypeDate:
+		return format.Date(m.Value.(time.Time))
+	case MetaTypeSize:
+		return format.Size(m.Value.(int64))
+	default:
+		return fmt.Sprint(m.Value)
+	}
 }
