779f92d7dc42eda4c3e635609f3606feaf5ded98
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-16 13:07:39 +0000

    fix(backend): remove redundant help display (#99)

--- backend/cmd/labstore-server/main.go
+++ backend/cmd/labstore-server/main.go
@@ -1,11 +1,11 @@
 package main
 
-import "github.com/IllumiKnowLabs/labstore/backend/pkg/helper"
+import "os"
 
 func main() {
 	rootCmd := NewRootCmd()
 
 	if err := rootCmd.Execute(); err != nil {
-		helper.CheckFatal(rootCmd.Help())
+		os.Exit(1)
 	}
 }
