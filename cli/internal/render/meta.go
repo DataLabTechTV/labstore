1ea9f0e3eda4bf193fddba433c766203b3c38a87
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-23 18:13:57 +0000

    feat(cli): extend the number type constraint with uint64 as well

--- cli/internal/render/meta.go
+++ cli/internal/render/meta.go
@@ -13,7 +13,7 @@ import (
 type Number interface {
 	~float32 | ~float64 |
 		~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 type Metadata map[string]Meta
