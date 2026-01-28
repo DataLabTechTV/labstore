7c0a26fa6ed393321e4064ce1a7a0c199077de30
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-01-28 19:56:58 +0000

    feat(cmd): add version commmand (#128)

---
+++ cmd/labstore/version.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/IllumiKnowLabs/labstore/cli/render"
+	"github.com/IllumiKnowLabs/labstore/server/constants"
+	"github.com/IllumiKnowLabs/labstore/server/router"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/spf13/cobra"
+)
+
+func NewVersionCmd() *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "version",
+		Short: "Display version and build details",
+		Run: func(cmd *cobra.Command, args []string) {
+			printWelcome()
+
+			labelStyle := lipgloss.NewStyle().
+				Foreground(render.ActivePalette.Accent).
+				Render
+
+			fmt.Printf(
+				"%s: %s (%s: %s, %s: %s)\n",
+				labelStyle("version"), constants.Version,
+				labelStyle("tag"), constants.GitTag,
+				labelStyle("commit"), constants.GitCommit,
+			)
+
+			fmt.Printf(
+				"%s: %s, %s: %s\n",
+				labelStyle("build time"), constants.BuildTime,
+				labelStyle("builder"), constants.Builder,
+			)
+
+			boolTrueStyle := lipgloss.NewStyle().
+				Foreground(render.ActivePalette.Success).
+				Render
+
+			boolFalseStyle := lipgloss.NewStyle().
+				Foreground(render.ActivePalette.Error).
+				Render
+
+			var embedAssetsView string
+			if router.EmbedAssets {
+				embedAssetsView = boolTrueStyle("yes")
+			} else {
+				embedAssetsView = boolFalseStyle("no")
+			}
+			fmt.Printf("%s: %v\n", labelStyle("embedded web assets"), embedAssetsView)
+		},
+	}
+
+	return cmd
+}
